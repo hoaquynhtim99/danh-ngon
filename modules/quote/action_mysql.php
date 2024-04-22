c01e30bf09ebef6d63b8b7b738ac252ca5c45e51
huydepzai121 <nguyenthehuy977943@gmail.com>
2024-04-22 18:30:50 +0700

    cập nhật lại csdl cho module danh ngôn

--- modules/quote/action_mysql.php
+++ modules/quote/action_mysql.php
@@ -14,15 +14,20 @@ if (!defined('NV_IS_FILE_MODULES')) {
 }
 
 $sql_drop_module = [];
+
 $sql_drop_module[] = "DROP TABLE IF EXISTS " . $db_config['prefix'] . "_" . $lang . "_" . $module_data;
+
 $sql_drop_module[] = "DROP TABLE IF EXISTS " . $db_config['prefix'] . "_" . $lang . "_" . $module_data . "_cats";
+
 $sql_drop_module[] = "DROP TABLE IF EXISTS " . $db_config['prefix'] . "_" . $lang . "_" . $module_data . "_authors";
 
 $sql_create_module = $sql_drop_module;
 
+// # 1. Bảng quản lý danh ngôn
 $sql_create_module[] = "CREATE TABLE " . $db_config['prefix'] . "_" . $lang . "_" . $module_data . " (
   id mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  catids varchar(250) NOT NULL DEFAULT '',
+  catids varchar(250) NOT NULL DEFAULT '' COMMENT 'ID Danh mục',
+  author_id mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'ID tác giả',
   content mediumtext NOT NULL,
   addtime int(11) unsigned NOT NULL DEFAULT '0',
   updatetime int(11) unsigned NOT NULL DEFAULT '0',
@@ -34,19 +39,19 @@ $sql_create_module[] = "CREATE TABLE " . $db_config['prefix'] . "_" . $lang . "_
   KEY status (status)
 ) ENGINE=InnoDB COMMENT 'Danh ngôn'";
 
+// # 2. Bảng quản lý tác giả
 $sql_create_module[] = "CREATE TABLE " . $db_config['prefix'] . "_" . $lang . "_" . $module_data . "_authors (
   id mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  title varchar(190) NOT NULL COMMENT 'Tiêu đề',
+  name_author varchar(190) NOT NULL DEFAULT '' COMMENT 'Tên tác giả',
   alias varchar(190) NOT NULL COMMENT 'Liên kết tĩnh không trùng',
   description text NOT NULL COMMENT 'Mô tả ngắn gọn',
   bodyhtml longtext NOT NULL COMMENT 'Chi tiết',
   image varchar(255) NOT NULL DEFAULT '' COMMENT 'Ảnh mô tả',
-  is_thumb tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0 là không có ảnh, 1 ảnh asset, 2 ảnh upload 3 ảnh remote',
   keywords text NOT NULL COMMENT 'Từ khóa, phân cách bởi dấu phảy',
   admin_id int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'ID người đăng',
-  add_time int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Thời gian thêm',
-  edit_time int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Thời gian sửa',
-  status tinyint(1) unsigned NOT NULL DEFAULT '0',
+  addtime int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Thời gian thêm',
+  updatetime int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Thời gian sửa',
+  status tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Trạng thái hoạt động: 0: Dừng, 1: Hoạt động',
   PRIMARY KEY (id),
   KEY catids (catids),
   KEY addtime (addtime),
@@ -54,16 +59,19 @@ $sql_create_module[] = "CREATE TABLE " . $db_config['prefix'] . "_" . $lang . "_
   KEY status (status)
 ) ENGINE=InnoDB COMMENT 'Tác giả danh ngôn'";
 
+// # 3. Bảng quản lý danh mục
 $sql_create_module[] = "CREATE TABLE " . $db_config['prefix'] . "_" . $lang . "_" . $module_data . "_cats (
   id smallint(4) unsigned NOT NULL AUTO_INCREMENT,
   nums int(11) unsigned NOT NULL DEFAULT '0',
   title varchar(190) NOT NULL DEFAULT '' COMMENT 'Tiêu đề',
   description text NOT NULL COMMENT 'Note',
-  add_time int(11) unsigned NOT NULL DEFAULT '0',
-  edit_time int(11) unsigned NOT NULL DEFAULT '0',
+  addtime int(11) unsigned NOT NULL DEFAULT '0',
+  updatetime int(11) unsigned NOT NULL DEFAULT '0',
   weight smallint(4) unsigned NOT NULL DEFAULT '0',
   status tinyint(4) NOT NULL DEFAULT '1' COMMENT '0: Dừng, 1: Hoạt động',
   PRIMARY KEY (id),
+  KEY addtime (addtime),
+  KEY updatetime (updatetime),
   KEY weight (weight),
   KEY status (status),
   UNIQUE KEY title (title)
